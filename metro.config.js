c84725cebc5628a6cab6aa42a6511e211cebdaf9
smhtrn10 <smhtrn10@gmail.com>
2026-04-05 12:07:01 +0300

    fix(metro): enable unstable_enablePackageExports to resolve zod subpaths

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,6 @@ const { withRorkMetro } = require("@rork-ai/toolkit-sdk/metro");
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.unstable_enablePackageExports = true;
+
 module.exports = withRorkMetro(config);
